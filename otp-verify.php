c74311312b8e87de25b91018ef3a98c05defda49
VanDal-360 <maitoomanenau@gmail.com>
2026-06-01 13:27:19 +1200

    Store last activity time on successful login
    Stores time of successful login for timeout purposes.

--- otp-verify.php
+++ otp-verify.php
@@ -48,6 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $_SESSION['user_id']       = $user['id'];
             $_SESSION['user_name']     = $user['name'];
             $_SESSION['role']          = $user['role'];
+            $_SESSION['last_activity'] = time();
 
             unset($_SESSION['pre_auth_user_id']);
 
