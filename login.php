ba455aacd803511130ea1d398e2bba848f8ecaf7
John_Robs <126650884+JohnRobs21@users.noreply.github.com>
2026-06-02 21:06:42 +1200

    minor update

--- login.php
+++ login.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
 session_set_cookie_params([
     'lifetime' => 1800,
     'httponly' => true,
@@ -10,7 +13,6 @@ require_once 'includes/security-headers.php';
 
 session_start();
 
-require_once __DIR__ . '/vendor/autoload.php';
 require_once 'config/db.php';
 require_once 'includes/auth.php';
 
